25c56dcef7d6c9ef04261c1d85bda1f62eb3d087
ltimmers2 <147807198+ltimmers2@users.noreply.github.com>
2023-10-13 10:30:00 +0200

    DevXP: Configured Terraform

--- devxp_main.tf
+++ devxp_main.tf
@@ -6,61 +6,7 @@ terraform {
   }
 }
 
-resource "aws_instance" "len" {
-      ami = data.aws_ami.ubuntu_latest.id
-      instance_type = "t2.micro"
-      tags = {
-        Name = "len"
-      }
-      lifecycle {
-        ignore_changes = [ami]
-      }
-}
 
-resource "aws_iam_user" "len_iam" {
-      name = "len_iam"
-}
-
-resource "aws_iam_user_policy_attachment" "len_iam_policy_attachment0" {
-      user = aws_iam_user.len_iam.name
-      policy_arn = aws_iam_policy.len_iam_policy0.arn
-}
-
-resource "aws_iam_policy" "len_iam_policy0" {
-      name = "len_iam_policy0"
-      path = "/"
-      policy = data.aws_iam_policy_document.len_iam_policy_document.json
-}
-
-resource "aws_iam_access_key" "len_iam_access_key" {
-      user = aws_iam_user.len_iam.name
-}
-
-data "aws_iam_policy_document" "len_iam_policy_document" {
-      statement {
-        actions = ["ec2:RunInstances", "ec2:AssociateIamInstanceProfile", "ec2:ReplaceIamInstanceProfileAssociation"]
-        effect = "Allow"
-        resources = ["arn:aws:ec2:::*"]
-      }
-      statement {
-        actions = ["iam:PassRole"]
-        effect = "Allow"
-        resources = [aws_instance.len.arn]
-      }
-}
-
-data "aws_ami" "ubuntu_latest" {
-      most_recent = true
-      owners = ["099720109477"]
-      filter {
-        name = "name"
-        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64*"]
-      }
-      filter {
-        name = "virtualization-type"
-        values = ["hvm"]
-      }
-}
 
 
 
